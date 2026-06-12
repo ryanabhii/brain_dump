af921e50097e7765b067729d5cf355cff6fa3420
Abhishek Kumar <abhishekkrsh39@gmail.com>
2026-06-12 14:11:28 +0000

    removed corrupted file

--- tracker_app\universal_tracker\android\app\src\main\kotlin\com\xenon54\tracker\MainActivity.kt
+++
@@ -1,5 +0,0 @@
-tracker_app\universal_tracker\android\app\src\main\kotlin\com\xenon54\package com.xenon54.tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
